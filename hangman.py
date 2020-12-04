dac960a9d48c69359d1af23f7cf4815cf9eff806
danilomizin <danilomizin@gmail.com>
2020-12-04 13:07:17 +0200

    Add function start, declare constant in the top of file, change print, fix when input Cyrillic letters

--- hangman.py
+++ hangman.py
@@ -1,21 +1,23 @@
 # Problem Set 2, hangman.py
 # Name:Mizin Danilo
-# Collaborators:Piustonen 
+# Collaborators:Piustonen
 # Time spent:
 
 import string
 import random
 
+
 WORDLIST_FILENAME = "words.txt"
 guesses_point=6
 warnings_point=3
 letters_guessed=[]
+letters_a = frozenset("aeiou")
 
 
 def load_words():
     """
     Returns a list of valid words. Words are strings of lowercase letters.
-    
+
     Depending on the size of the word list, this function may
     take a while to finish.
     """
@@ -30,15 +32,16 @@ def load_words():
     return wordlist
 
 
-wordlist = load_words()
-
 def choose_word(wordlist):
     """
     wordlist (list): list of words (strings)
-    
+
     Returns a word from wordlist at random
     """
     return random.choice(wordlist)
+
+
+wordlist = load_words()
 
 
 def is_word_guessed(secret_word, letters_guessed):
@@ -52,7 +55,6 @@ def is_word_guessed(secret_word, letters_guessed):
     '''
     # FILL IN YOUR CODE HERE AND DELETE "pass"
     pass
-
 
 def get_guessed_word(secret_word, letters_guessed):
     '''
@@ -73,21 +75,18 @@ def get_available_letters(letters_guessed):
     '''
     # FILL IN YOUR CODE HERE AND DELETE "pass"
     pass
-    
-    
 
 
-def warnings(value_error):
+def warnings(value_mistake):
     """
     Subtract warnings point and output name of error
     """
     global warnings_point
     global guesses_point
-    format_error="Oops! {}".format(value_error)
+    name_mistake=f"Oops! {value_mistake}"
     if warnings_point>0:
         warnings_point-=1
-        print(format_error,"\nYou have", warnings_point, "warnings left:",end=" ")
-
+        print(name_mistake,"\nYou have", warnings_point, "warnings left:",end=" ")
     else:
         guesses_point-= 1
         print("Oops! You have no warnings left, so you lose one guess:", end=' ')
@@ -110,26 +109,27 @@ def letter_in_word(secret_word,letter,letters_a):
         print("Oops! That letter is not in my word:", get_guessed_word(secret_word, letters_guessed))
 
 
-def hangman(secret_word, request):
+def hangman(secret_word, request,letters_a,warnings_point):
     '''
     Game Hangman. "request" show hangman(classic) or hangman with hints.
     Print information for user, start loop, check user choose hangman or hangman with hints,
     Check input letter and call letter_in_word, check if user guessed word.
     '''
     global letters_guessed
-    global warnings_point
+    #global warnings_point
     global guesses_point
-    letters_a = frozenset("aeiou")
     print("I am thinking of a word that is", len(secret_word), "letters long")
     print("You have", warnings_point, "warnings left")
     while guesses_point > 0:
-        print("-------\nYou have", guesses_point, "guesses left\nAvailable letters:", end=" ")
-        print(get_available_letters(letters_guessed))
+        print("-------")
+        print("You have", guesses_point, "guesses left")
+        print("Available letters:", end=" ")
+        get_available_letters(letters_guessed)
         letter = input("Please guess a letter:")
-        if request == "hints" and letter=="*":
+        if request is True and letter=="*":
             show_possible_matches(get_guessed_word(secret_word, letters_guessed))
         else:
-            if str.isalpha(letter.lower()) == False or len(letter) != 1:
+            if letter not in string.ascii_letters or len(letter) != 1:
                 warnings("That is not a valid letter or you print more than one letter.")
                 print(get_guessed_word(secret_word, letters_guessed))
             elif letter in letters_guessed:
@@ -139,28 +139,29 @@ def hangman(secret_word, request):
                 letter_in_word(secret_word, letter, letters_a)
         if is_word_guessed(secret_word, letters_guessed) == 0:
             final_points = guesses_point * len(set(secret_word))
-            print("------\nCongratulations, you won!\nWord:",secret_word,"\nYour total score for this game is:", final_points)
+            print("------")
+            print("Congratulations, you won!")
+            print("Word:",secret_word)
+            print("Your total score for this game is:", final_points)
             guesses_point = -1
         elif is_word_guessed(secret_word, letters_guessed) != 0 and guesses_point <= 0:
-            print("------\nSorry, you ran out of guessed. The word was", secret_word)
+            print("------")
+            print("Sorry, you ran out of guessed. The word was", secret_word)
             break
-
 
 def match_with_gaps(my_word, other_word):
     '''
     my_word: string with _ characters, current guess of secret word
     other_word: string, regular English word
-    returns: boolean, True if all the actual letters of my_word match the 
+    returns: boolean, True if all the actual letters of my_word match the
         corresponding letters of other_word, or the letter is the special symbol
         _ , and my_word and other_word are of the same length;
-        False otherwise: 
+        False otherwise:
     '''
-    word = my_word.replace(" ", "")
-    if len(word) == len(other_word):
-        for i in range(len(word)):
-            if word[i] == '_' or word[i] == other_word[i]:
-                continue
-            else:
+    guesses_word = my_word.replace(" ", "")
+    if len(guesses_word) == len(other_word):
+        for i in range(len(guesses_word)):
+            if guesses_word[i] != '_' and guesses_word[i] != other_word[i]:
                 return False
         return True
     else:
@@ -184,13 +185,21 @@ def show_possible_matches(my_word):
     print(" ".join(list_with_words))
 
 
-if __name__ == "__main__":
+def start():
     print("Welcome to the game Hangman!\nDo you want play hangman (classic) or hangman with hints?")
     while type:
-        request=input("if you want to play hangman (classic) input 'classic', play hangman with hints input 'hints':")
-        if request=="classic" or request=="hints":
+        request = input("if you want to play hangman (classic) input 'classic', play hangman with hints input 'hints':").replace(" ","")
+        if request == "classic" or request == "hints":
+            if request=='hints':
+                request=True
+            else:
+                request=False
             secret_word = choose_word(wordlist)
-            hangman(secret_word, request)
+            hangman(secret_word, request, letters_a,warnings_point)
             break
         else:
             print("Oops! Your input is not correct")
+
+
+if __name__ == "__main__":
+    start()
